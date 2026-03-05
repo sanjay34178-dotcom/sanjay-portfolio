b40e54c6393e68492d16f93767941ef9a57f710a
sanjay <sanjay34178@gmail.com>
2026-03-05 20:10:28 +0530

    Delete Portfolio directory

--- Portfolio/script.js
+++
@@ -1,8 +0,0 @@
-function openImage(img){
-document.getElementById("imagePopup").style.display="flex"
-document.getElementById("popupImg").src=img.src
-}
-
-function closeImage(){
-document.getElementById("imagePopup").style.display="none"
-}
